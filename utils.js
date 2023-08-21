6f0f311f99f1de9a0b390f9273c58752401dfef0
micserv010821 <fullstack290623@gmail.com>
2023-08-21 20:33:50 +0300

    .

---
+++ utils.js
@@ -0,0 +1,13 @@
+function get_random(max_random) {
+    return Math.floor(Math.random() * max_random) + 1
+}
+
+// take code from array_loops.html
+// create a function get_max --> which gets an array as a parameter and returns the max value
+// create a function get_min --> which gets an array as a parameter and returns the min value
+// add the populate_list_random into this file utils.js
+
+// create an HTML page, invite the utils.js (script src...) 
+// create a random array of 20 items with random of numbers between 1-1000
+// call get_max and print its result
+// call get_min and print its result
